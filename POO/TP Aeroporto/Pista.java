4d609e63c08eb9fbbf985406d82e05f086f2b99e
Jonathan Alexandre Rodrigues Alves <91800109+Jhon2910@users.noreply.github.com>
2026-05-21 10:34:02 -0300

    Create Pista.java

---
+++ POO/TP Aeroporto/Pista.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Pista{
+
+        private String pouso; 
+
+        Queue<Aviao> filaPouso1 = new LinkedList<>();
+        Queue<Aviao> filaPouso2 = new LinkedList<>();
+        Queue<Aviao> filaPouso3 = new LinkedList<>();
+        Queue<Aviao> filaPouso4 = new LinkedList<>();
+
+        public pista(String pouso,){
+
+        }
+
+}
