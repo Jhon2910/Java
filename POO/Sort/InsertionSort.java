9dfb61ee7f8789465244815b2591d4f2c4f59495
Jonathan Alexandre Rodrigues Alves <91800109+Jhon2910@users.noreply.github.com>
2026-08-18 11:56:12 -0300

    Create InsertionSort.java

---
+++ POO/Sort/InsertionSort.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int v[] = {5, 2, 4, 6, 1, 3};
+
+        for (int i = 1; i < v.length; i++) {
+            int carta = v[i];//2
+            int aux = i - 1;//0
+
+            while (aux >= 0 && v[aux] > carta) {
+                v[aux + 1] = v[aux];
+                aux = aux - 1;
+            }
+            v[aux + 1] = carta;
+        }
+
+        for (int i = 0; i <= v.length - 1; i++) {
+            System.out.print(" " + v[i] + " ");
+        }
+    }
+}
